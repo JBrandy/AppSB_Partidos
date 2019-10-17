c3f84ca38466ff2244ba2f7effe4ef9a8b6de202
JBrandy <jesusbrandygarcia@gmail.com>
2019-10-17 18:26:28 +0200

    version 1 clase

---
+++ PruebaJavaFX/src/brandy/controladores/DialogoPartidosFxmControllr.java
@@ -0,0 +1,77 @@
+package brandy.controladores;
+
+import brandy.logica.Logica;
+import brandy.models.Division;
+import brandy.models.Partido;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DialogoPartidosFxmControllr {
+
+    @FXML
+    private TextField tfLocal;
+
+    @FXML
+    private TextField tfVisitante;
+
+    @FXML
+    private TextField tfrLocal;
+
+    @FXML
+    private TextField tfrVisitante;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private ComboBox<Division> division;
+
+    @FXML
+    private Button btAceptar;
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    void addPartido(ActionEvent event) {
+
+        LocalDate localDate = (LocalDate) date.getValue();
+        Date date3 = Date.from(localDate.atStartOfDay(
+                ZoneId.systemDefault()).toInstant());
+
+        int resultado_Lo = Integer.parseInt(tfrLocal.getText());
+        int resultado_Vi = Integer.parseInt(tfrVisitante.getText());
+
+        String sA = String.valueOf(resultado_Lo);
+        String sB = String.valueOf(resultado_Vi);
+        String resultado = sA + "-" + sB;
+        Partido p = new Partido(tfLocal.getText(),tfVisitante.getText(),(Integer.parseInt(tfrLocal.getText())),(Integer.parseInt(tfrVisitante.getText())),division.getValue(), resultado,date3);
+        Logica.getInstance().addPartida(p);
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        stage.close();
+
+
+
+
+    }
+
+
+
+    @FXML
+    void cancelar(ActionEvent event) {
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        stage.close();
+    }
+
+}
--- PruebaJavaFX/src/brandy/controladores/MainController.java
+++ PruebaJavaFX/src/brandy/controladores/MainController.java
@@ -5,14 +5,21 @@ import brandy.logica.Logica;
 import brandy.models.Division;
 import brandy.models.Partido;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -42,6 +49,23 @@ public class MainController implements Initializable {
     @FXML
     private Button btAnadir;
 
+
+    @FXML
+    void generar(ActionEvent event){
+        Parent root = null;
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        try {
+            root = FXMLLoader.load(getClass().getResource("/brandy/vistas/DialogoPartidoFxm.fxml"));
+            stage.setTitle("Pantalla principal");
+            stage.setScene(new Scene(root, 300, 300));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -50,6 +74,7 @@ public class MainController implements Initializable {
 
 
     }
+
 
 
     }
